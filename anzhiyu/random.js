d540dc3f32df4caaf08556878fa7e10f6d9363ce
ccycc <shoneyoung@126.com>
2025-05-25 18:58:12 +0800

    Site updated: 2025-05-25 18:58:11

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/25/hello-bt/","2025/05/25/hello-git/","2025/05/24/hello-web/","2025/05/24/hello-world/"];function toRandomPost(){
+var posts=["2025/05/25/hello-bt/","2025/05/24/hello-world/","2025/05/25/hello-git/","2025/05/24/hello-web/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
