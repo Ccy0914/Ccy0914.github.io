65a2f54f0d63b70f01f63d7f1d2480b27b3390c5
ccycc <3546216350@qq.com>
2025-05-27 22:08:25 +0800

    Site updated: 2025-05-27 22:08:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/25/hello-bt/","2025/05/25/hello-git/","2025/05/24/hello-web/","2025/05/25/hello-hub/","2025/05/24/hello-world/","2024/03/14/msf-eternal-blue-tutorial/"];function toRandomPost(){
+var posts=["2025/05/25/hello-bt/","2025/05/25/hello-git/","2025/05/25/hello-hub/","2025/05/24/hello-web/","2024/03/14/msf-eternal-blue-tutorial/","2025/05/24/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
