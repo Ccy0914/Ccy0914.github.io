2f449c9fa7bc8e6ecf3bcd378139f30f9f88e394
ccycc <3546216350@qq.com>
2025-05-31 00:02:25 +0800

    Site updated: 2025-05-31 00:02:16

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/27/linux-system-commands/","2025/05/28/ultimate-ai-toolbox-guide/","2025/05/27/msf-eternal-blue-tutorial/"];function toRandomPost(){
+var posts=["2025/05/27/linux-system-commands/","2025/05/30/dvwa-web-security-guide/","2024/03/17/msf-eternal-blue-tutorial/","2025/05/30/sqli-lab-guide/","2025/05/28/ultimate-ai-toolbox-guide/","2025/05/30/upload-labs-guide/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
