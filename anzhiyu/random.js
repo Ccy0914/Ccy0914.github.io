dd48e4fe70d5325e054af4a06e54dcea1cdbdfad
ccycc <3546216350@qq.com>
2025-06-04 11:33:52 +0800

    Site updated: 2025-06-04 11:33:48

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/05/25/hello-bt/","2025/05/25/hello-git/","2025/05/25/hello-web/","2025/05/25/hello-world/","2025/05/25/hello-hub/"];function toRandomPost(){
+var posts=["2025/05/25/hello-bt/","2025/05/25/hello-git/","2025/05/25/hello-hub/","2025/05/25/hello-world/","2025/05/25/hello-web/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
